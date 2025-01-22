ad8b19e489f4ef060195850ab8d1bc86c982aa15
Swathi <51788517+swathi2406@users.noreply.github.com>
2025-01-22 09:16:29 +0530

    Time: 2 ms (23.15%) | Memory: 8.3 MB (92.72%) - LeetSync

--- 169-majority-element/majority-element.go
+++ 169-majority-element/majority-element.go
@@ -1,18 +1,16 @@
 func majorityElement(nums []int) int {
 
 	var maps = make(map[int]int)
-
+    max := 0
+	val := 0
 	for _, num := range nums {
 		maps[num] = maps[num] + 1
-	}
+        if max < maps[num]{
+            max = maps[num]
+            val = num
 
-	max := 0
-	val := 0
-	for index, freq := range maps {
-		if max < freq {
-			max = freq
-			val = index
-		}
+
+        }
 	}
 	return val
 
