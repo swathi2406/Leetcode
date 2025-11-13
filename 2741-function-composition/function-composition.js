9f44611cfd1c001649d22aa4dd64276b8701a756
Swathi <51788517+swathi2406@users.noreply.github.com>
2025-11-13 11:02:08 +0530

    Time: 57 ms (62.37%) | Memory: 56.6 MB (27.94%) - LeetSync

---
+++ 2741-function-composition/function-composition.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    return function(x) {
+        if (functions.length == 0){
+            return x
+        }
+        let y = 0;
+        for (let i=functions.length-1;i>=0;i--){
+            y= functions[i](x);
+            x = y;
+        }
+        
+        return y;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
