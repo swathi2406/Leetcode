356ccb4ad82465e932cc173c5163ac33115a3921
Swathi <51788517+swathi2406@users.noreply.github.com>
2025-11-30 15:05:20 +0530

    Time: 44 ms (16.00%) | Memory: 17.9 MB (32.90%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -3,8 +3,7 @@ class Solution:
         length = min(len(word1),len(word2))
         res = ""
         for i in range(length):
-            res +=word1[i]
-            res +=word2[i]
+            res +=word1[i] + word2[i]
 
         if len(word1)> length:
             res +=word1[length:]
