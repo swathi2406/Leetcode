a93e2a94e8a03b29159e40b645d3b4492bb0e29c
Swathi <51788517+swathi2406@users.noreply.github.com>
2025-12-04 06:46:18 +0530

    Time: 3 ms (99.79%) | Memory: 18.2 MB (16.47%) - LeetSync

--- 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.py
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.py
@@ -1,13 +1,8 @@
 class Solution:
     def maxDistinct(self, s: str) -> int:
-        dict_freq_substring = {}
         count = 0
-        for i in s:
-            if i in dict_freq_substring.keys():
-                dict_freq_substring[i] +=1
-            else:
-                dict_freq_substring[i] = 1
-        for strs in dict_freq_substring.keys():
-            count +=1
+        for strs in "abcdefghijklmnopqrstuvwxyz":
+            if strs in s:
+                count +=1
         return count
         
